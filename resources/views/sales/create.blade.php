3b57f326a0afeaaa04bc7d970001076936e986f6
Thouhidul Islam <thouhid@magnetismtech.com>
2025-11-21 18:57:02 +0600

    wip

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -12,15 +12,6 @@
 
 <form action="{{ route('sales.store') }}" method="POST">
     @csrf
-    {{-- <div class="form-group">
-        <label for="medicine_id">Medicine</label>
-        <select name="medicine_id" id="medicine_id" class="form-control" required>
-            <option value="">-- Select Medicine --</option>
-            @foreach($medicines as $medicine)
-                <option value="{{ $medicine->id }}">{{ $medicine->name }}</option>
-            @endforeach
-        </select>
-    </div> --}}
 
     <div class="mb-2">
         <label>Medicine</label>
@@ -32,20 +23,13 @@
                 <option data-id="{{ $medicine->id }}" value="{{ $medicine->name }}"></option>
             @endforeach
         </datalist>
-
         <!-- Hidden input to store ID -->
         <input type="hidden" name="medicine_id" id="medicine_id" required>
     </div>
-
     <div class="form-group">
         <label for="quantity">Quantity</label>
         <input type="number" name="quantity" id="quantity" class="form-control" min="1" required>
     </div>
-
-    {{-- <div class="form-group">
-        <label for="selling_price">Selling Price</label>
-        <input type="number" name="selling_price" id="selling_price" class="form-control" step="0.01" required>
-    </div> --}}
     <div class="mb-3">
         <label>Total Price</label>
         <input type="number" step="0.01" id="total_price" name="total_price" class="form-control" required>
